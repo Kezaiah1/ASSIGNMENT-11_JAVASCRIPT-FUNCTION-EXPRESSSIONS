bd61c2012f662347b87161150c4490ad3c9a888b
Kezaiah Fixon-Owoo <fixonowookezaiah@gmail.com>
2024-07-17 02:40:14 +0000

    ASSIGNMENT 11(FIXON-OWOO KEZAIAH)_JS FUNCTION EXPRESSIONS

---
+++ index.js
@@ -0,0 +1,28 @@
+const greeting = function(){
+    console.log("Hello!");
+}
+greeting();
+
+
+let count = 0
+document.getElementById("increaseButton").onclick = function(){
+    count+=1;
+    document.getElementById("myLabel").innerHTML = count;
+}
+
+document.getElementById("decreaseButton").onclick = function(){
+    count-=1;
+    document.getElementById("myLabel").innerHTML = count;
+}
+
+/*
+function increaseCount(){
+    count+=1;
+    document.getElementById("myLabel").innerHTML = count;
+}
+
+function decreaseCount(){
+    count-=1;
+    document.getElementById("myLabel").innerHTML = count;
+}
+*/
